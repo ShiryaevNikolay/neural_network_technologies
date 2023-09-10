5201599d057b808c527df2aba156b1802cbd84a6
Nikolay Shiryaev <deendeeps@list.ru>
2023-09-10 20:41:52 +0300

    фикс чтения весов

--- lab1/utils/write_data.py
+++ lab1/utils/write_data.py
@@ -11,10 +11,10 @@ def save_weights(weights, sheet_name='weights', file_name='weights.xlsl'):
     weights_file = load_workbook(file_name)
     weights_file.create_sheet(sheet_name)
     sheet = weights_file.get_sheet_by_name(sheet_name)
-    row_length = len(weights)
-    for i in range(row_length):
-        column_length = weights[i]
-        for j in range(column_length):
+    column_length = len(weights)
+    for i in range(column_length):
+        row_length = len(weights[i])
+        for j in range(row_length):
             sheet.cell(column=i + 1, row=j + 1).value = weights[i][j]
     weights.save(file_name)
     weights.close()
