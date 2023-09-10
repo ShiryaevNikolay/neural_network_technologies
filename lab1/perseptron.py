84a60ee1c61b3fd209919eb4c242f6712f730c0d
Nikolay Shiryaev <deendeeps@list.ru>
2023-09-10 20:42:02 +0300

    функция обучения

--- lab1/perseptron.py
+++ lab1/perseptron.py
@@ -2,6 +2,7 @@ from utils.init import init_weights
 from utils.func import net
 from utils.read_data import read_weights
 from utils.write_data import save_weights
+import numpy as np
 
 _file_name = 'weights.xlsx'
 _sheet_name = 'weigths'
@@ -15,6 +16,7 @@ class Perseptron:
         :param activate_fun: Активационная функция
         """
         self.shift = shift
+        self.learn_speed = np.random.uniform(0.5, 0.7)  # Скорость обучения
         self.activate_fun = activate_fun
         weights = read_weights(length, file_name=_file_name, sheet_name=_sheet_name)
         if len(weights) == 0:
@@ -28,8 +30,16 @@ class Perseptron:
         :param values: Вектор входных данных
         :return: Результат активационной функции
         """
-        sum_value = net(values, self.weights) + self.shift
+        weights = self.weights.flatten()
+        input_data = values.flatten()
+        sum_value = net(input_data, weights) + self.shift
         return self.activate_fun(sum_value)
 
     def train(self, values):
-        pass
+        result = self.result(values)
+        error = target - result
+        column_length = len(self.weights)
+        for i in range(column_length):
+            row_length = len(self.weights[i])
+            for j in range(row_length):
+                self.weights[i][j] += error * values[i][j] * self.learn_speed
