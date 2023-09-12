4420b19ae8e9d2dff76cc61817a82206d44b7104
Nikolay Shiryaev <deendeeps@list.ru>
2023-09-12 21:11:57 +0300

    логика выбора действия

--- lab1/main.py
+++ lab1/main.py
@@ -1,6 +1,34 @@
 from app.app import App
+from perseptron import Perseptron
+from utils.activate_func import treshold
+from train import train_perseptron
 
 _size = 28
 
-app = App(size=_size)
-app.mainloop()
+perseptron = Perseptron(length=_size, shift=0.5, activate_fun=treshold)
+
+
+def get_perseptron_result():
+    pass
+
+
+while True:
+    info_text = """
+    Что планируется сделать?
+    1. Обучить персептрон
+    2. Открыть приложение, чтобы нарисовать фигуру
+    
+    Введите цифру: 
+    """
+    try:
+        action = int(input(info_text))
+    except:
+        continue
+    if action == 1 or action == 2:
+        break
+
+if action == 1:
+    train_perseptron(perseptron)
+elif action == 2:
+    app = App(size=_size, get_result_callback=get_perseptron_result)
+    app.mainloop()
