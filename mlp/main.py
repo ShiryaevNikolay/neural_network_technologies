5cca78aa7622ab8efa3b142a99802ed53a63d354
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-10-10 21:17:49 +0300

    Правки модели

--- mlp/main.py
+++ mlp/main.py
@@ -8,18 +8,22 @@ import matplotlib.pyplot as plt
 from func import to_category_dict
 
 file_name = "pakwheels_used_cars.csv"
-data_frame = pandas.read_csv(file_name)
-data_frame.dropna(subset=["price"], inplace=True)
-input_names = ["body", "make", "price"]
-output_names = ["assembly"]  # Импортирована или нет
 
-print(to_category_dict(data_frame["body"]))
+body = "body"
+make = "make"
+price = "price"
+assembly = "assembly"  # Импортирована или нет
+
+data_frame = pandas.read_csv(file_name)
+data_frame.dropna(subset=[price], inplace=True)
+input_names = [body, make, price]
+output_names = [assembly]  # Импортирована или нет
 
 max_price = data_frame["price"].max()
-encoders = {"price": lambda price: [price/max_price],
-            "body": lambda body: to_category_dict(data_frame["body"]).get(body),
-            "make": lambda make: to_category_dict(data_frame["make"]).get(make),
-            "assembly": lambda assembly: {"Imported": [1], np.NaN: [0]}.get(assembly)}
+encoders = {price: lambda value: [value/max_price],
+            body: lambda value: to_category_dict(data_frame[body]).get(value),
+            make: lambda value: to_category_dict(data_frame[make]).get(value),
+            assembly: lambda value: {"Imported": [1], np.NaN: [0]}.get(value)}
 
 
 def data_frame_to_dict(df):
@@ -66,17 +70,20 @@ supervised = make_supervised(data_frame)
 encoded_inputs = np.array(encode(supervised["inputs"]))
 encoded_outputs = np.array(encode(supervised["outputs"]))
 
-train_x = encoded_inputs[:600]
-train_y = encoded_outputs[:600]
+train_index = 5000
 
-test_x = encoded_inputs[600:]
-test_y = encoded_outputs[600:]
+train_x = encoded_inputs[:train_index]
+train_y = encoded_outputs[:train_index]
+
+test_x = encoded_inputs[train_index:]
+test_y = encoded_outputs[train_index:]
 
 model = keras.Sequential()
 # Dence - полносвязанная сеть
 # units - кол-во
-model.add(keras.layers.Dense(units=5, activation="relu"))  # TODO: кол-во units = кол-во столбцов во входных данных
-model.add(keras.layers.Dense(units=1, activation="sigmoid"))  # TODO: units=1, тк выход 0 или 1
+model.add(keras.layers.Dense(units=64, activation="relu"))
+model.add(keras.layers.Dense(units=64, activation="relu"))
+model.add(keras.layers.Dense(units=1, activation="sigmoid"))
 # Компиляция
 # lass - функция потерь. mse - среднеквадратичная ошибка
 # optimizer: sgd - стахостический градиентный спуск
@@ -86,7 +93,7 @@ model.compile(loss="mse", optimizer="sgd", metrics=["accuracy"])
 # model.load_weights("weights.h5")
 # Тренеровка
 # validation_split означает 80% данных на тренировку и 20% на валидацию
-fit_results = model.fit(x=train_x, y=train_y, epochs=10, validation_split=0.2)
+fit_results = model.fit(x=train_x, y=train_y, batch_size=64, epochs=1000, validation_split=0.2)
 
 plt.title("Losses train/validation")
 plt.plot(fit_results.history["loss"], label="Train")
@@ -102,7 +109,7 @@ plt.show()
 
 predicted_test = model.predict(test_x)
 # iloc - берем срез по определенному индексу
-real_data = data_frame.iloc[600:][input_names + output_names]
+real_data = data_frame.iloc[train_index:][input_names + output_names]
 real_data["PAssembly"] = predicted_test
 print(real_data)
 
